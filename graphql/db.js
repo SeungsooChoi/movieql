06158e53770dd8b7c9fb5a29727093464284dc21
SeungsooChoi <tmdtn5389@gmail.com>
2021-08-23 18:13:37 +0900

    Feat : Learning Mutation(add, delete)

--- graphql/db.js
+++ graphql/db.js
@@ -1,31 +1,49 @@
-export const people = [
+let movies = [
+  {
+    id: 0,
+    name: "Star wars",
+    score: 99,
+  },
   {
     id: 1,
-    name: "Seungsoo",
-    age: 27,
-    gender: "male",
+    name: "Avengers",
+    score: 97,
   },
   {
     id: 2,
-    name: "Jiwoo",
-    age: 21,
-    gender: "female",
+    name: "Iron man3",
+    score: 92,
   },
   {
     id: 3,
-    name: "Ahin",
-    age: 17,
-    gender: "male",
-  },
-  {
-    id: 4,
-    name: "Woojoo",
-    age: 23,
-    gender: "male",
+    name: "Logan",
+    score: 100,
   },
 ];
+
+export const getMovies = () => movies;
 
 export const getById = (id) => {
   const filteredPeople = people.filter((person) => id === person.id);
   return filteredPeople[0];
+};
+
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter((movie) => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score,
+  };
+  movies.push(newMovie);
+  return newMovie;
 };
